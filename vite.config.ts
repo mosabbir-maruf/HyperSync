1e175acf76f4504f23ac4753a5a5209a99d4c443
Mosabbir Maruf <96982516+mosabbir-maruf@users.noreply.github.com>
2026-08-07 04:03:50 +0600

    fix: update navigateFallback to '/index.html' for improved routing

--- vite.config.ts
+++ vite.config.ts
@@ -77,7 +77,7 @@ export default defineConfig(({ mode }) => {
           ],
         },
         workbox: {
-          navigateFallback: '/',
+          navigateFallback: '/index.html',
           navigateFallbackDenylist: [
             /^\/api/, // VERY IMPORTANT: Do not intercept API requests
           ],
